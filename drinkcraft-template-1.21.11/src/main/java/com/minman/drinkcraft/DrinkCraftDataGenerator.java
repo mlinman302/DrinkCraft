18ae7afadc8f7446bb4ed379982ba020224514f3
[Max Inman] <max.inman101@gmail.com>
2026-02-07 19:06:35 -0500

    add basic fabric mod template

---
+++ drinkcraft-template-1.21.11/src/main/java/com/minman/drinkcraft/DrinkCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package com.minman.drinkcraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DrinkCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
